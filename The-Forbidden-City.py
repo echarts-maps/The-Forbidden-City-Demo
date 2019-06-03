a6341d40f9be8cb842efa2880bab6ac1d4f25a02
chfw <wangc_2011@hotmail.com>
2019-06-03 23:51:53 +0100

    step 5: sharing it

--- The-Forbidden-City.py
+++ The-Forbidden-City.py
@@ -2,13 +2,13 @@ from pyecharts import options as opts
 from pyecharts.charts import Geo
 from pyecharts.datasets import register_url
 
-register_url("http://127.0.0.1:8000")
+register_url("https://echarts-maps.github.io/The-Forbidden-City-Demo")
 
 g = (
-       Geo()
-        .add_schema(maptype="The Forbidden City")
-        .set_global_opts(
-            title_opts=opts.TitleOpts(title="The Forbidden City in Beijing"),
-        )
+    Geo()
+    .add_schema(maptype="The Forbidden City")
+    .set_global_opts(
+        title_opts=opts.TitleOpts(title="The Forbidden City in Beijing"),
+    )
 )
 g.render()
